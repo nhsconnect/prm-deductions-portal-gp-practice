7cfffd8e0e5d0ac2b1247dbb7f492bb909e730a4
Phil Evans <phil.evans@thoughtworks.com>
2019-10-29 16:34:37 +0000

    [PRMT-557] Move buildspec to root directory

--- terraform/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "deductions-web-igw" {
-  vpc_id = "${aws_vpc.deductions-web-vpc.id}"
-
-  tags = {
-    Name = "deductions-web-igw"
-  }
-}
--- terraform/vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "deductions-web-vpc" {
-    cidr_block = "${var.deductions_vpc_cidr}"
-
-    tags = {
-        Name = "deductions-web-vpc"
-    }
-}
