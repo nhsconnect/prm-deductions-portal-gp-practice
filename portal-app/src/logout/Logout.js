b5ac8c198a040b31abf345b9a45076c6dc0eb6b8
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2019-11-15 14:37:42 +0000

    [PRMT-492] Styled log out button

--- portal-app/src/logout/Logout.js
+++ portal-app/src/logout/Logout.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import styles from "../app/App.module.scss";
 
 const Logout = ({logout}) => {
   return (
-    <div>
       <div className="nhsuk-header__transactional-service-name">
-        <button className="nhsuk-button nhsuk-button--reverse" onClick={logout}>Log Out</button>
+        <button id={styles.logout} className="nhsuk-button nhsuk-button--reverse" onClick={logout}>Log Out</button>
       </div>
-    </div>
   );
 };
 
 export default Logout;
+
