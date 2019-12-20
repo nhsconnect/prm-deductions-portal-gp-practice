78c3bb57569d8d9137485f887d475d590fa8c42a
bfisher <fishey2b@gmail.com>
2019-12-20 18:28:26 +0000

    [PMRT-474] Removed babel/preset-env and adapted import statements for server tests

--- server/app.test.js
+++ server/app.test.js
@@ -1,5 +1,5 @@
-import request from 'supertest';
-import app from './app';
+const request = require('supertest');
+const app = require('./app');
 
 describe('app test', () => {
   it('/user', (done) => {
--- server/server.integration.test.js
+++ server/server.integration.test.js
@@ -1,4 +1,4 @@
-import portscanner from "portscanner";
+const portscanner = require("portscanner");
 
 const TEST_PORT = 5000;
 const TEST_HOST = "127.0.0.1";
