6b9f442b4343e6a929e3421f5d3e8ee0e124b160
Wendy Lee <wendy.lee@thoughtworks.com>
2020-01-07 11:22:43 +0000

    [PRMT-474] Update config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
-const NHSIdentitySandpitLogInUrl = process.env.REACT_APP_GP_PORTAL_IDENTITY_URL
+const config = {
+  NHSIdentityUrl: process.env.REACT_APP_GP_PORTAL_IDENTITY_URL,
+  NHSIdentityClientId:process.env.REACT_APP_GP_PORTAL_CLIENT_ID,
+  NHSIdentityUserInfo:process.env.REACT_APP_GP_PORTAL_USER_INFO,
+  NHSIdentityRedirectUri:process.env.REACT_APP_GP_PORTAL_REDIRECT_URI
+};
 
-export default NHSIdentitySandpitLogInUrl
+module.exports= config;
