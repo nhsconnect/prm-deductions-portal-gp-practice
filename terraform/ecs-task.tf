d2cf7543f36f5af8f4f1bbafc115602e79a37c59
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-09 09:17:33 +0000

    [PRMT-474] Fix environment variables setup in ecs task

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -2,12 +2,12 @@ locals {
     task_execution_role     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.task_execution_role}"
     task_ecr_url            = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
     task_log_group          = "/nhs/deductions/${var.environment}-${data.aws_caller_identity.current.account_id}/${var.task_family}"
-        environment_variables        = [
-          { name = "REACT_APP_GP_PORTAL_IDENTITY_URL", value = data.aws_ssm_parameter.identity_url.value },
-          { name = "REACT_APP_GP_PORTAL_REDIRECT_URI", value = data.aws_ssm_parameter.redirect_uri.value },
-          { name = "REACT_APP_GP_PORTAL_USER_INFO", value = data.aws_ssm_parameter.user_info.value },
-          { name = "REACT_APP_GP_PORTAL_CLIENT_ID", value = data.aws_ssm_parameter.client_id.value }
-        ]
+    environment_variables        = [
+      { name = "REACT_APP_GP_PORTAL_IDENTITY_URL", value = data.aws_ssm_parameter.identity_url.value },
+      { name = "REACT_APP_GP_PORTAL_REDIRECT_URI", value = data.aws_ssm_parameter.redirect_uri.value },
+      { name = "REACT_APP_GP_PORTAL_USER_INFO", value = data.aws_ssm_parameter.user_info.value },
+      { name = "REACT_APP_GP_PORTAL_CLIENT_ID", value = data.aws_ssm_parameter.client_id.value }
+    ]
 }
 
 resource "aws_ecs_task_definition" "task" {
@@ -17,9 +17,8 @@ resource "aws_ecs_task_definition" "task" {
   cpu                       = var.task_cpu
   memory                    = var.task_memory
   execution_role_arn        = local.task_execution_role
-  
 
-  container_definitions     =  templatefile("${path.module}/templates/ecs-task-def.tmpl", { 
+  container_definitions     =  templatefile("${path.module}/templates/ecs-task-def.tmpl", {
         container_name  = var.task_container_name,
         ecr_url         = local.task_ecr_url
         image_name      = var.task_image_name
@@ -29,6 +28,7 @@ resource "aws_ecs_task_definition" "task" {
         container_port  = var.task_container_port,
         host_port       = var.task_host_port,
         log_region      = var.region,
-        log_group       = local.task_log_group
+        log_group       = local.task_log_group,
+        environment_variables = local.environment_variables
     })
 }
