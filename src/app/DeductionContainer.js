68ae1a931ab583d631fdcbb921b1a2b7fff9ea20
Wendy Lee <wendy.lee@thoughtworks.com>
2020-01-13 12:21:09 +0000

    [PRMT-474] Change status list from private router to normal router.

--- src/app/DeductionContainer.js
+++ src/app/DeductionContainer.js
@@ -1,42 +1,15 @@
 /* istanbul ignore file */
 import React from "react";
-import {Route, Switch, useHistory, Redirect} from "react-router-dom";
+import {Route, Switch, useHistory} from "react-router-dom";
 import DeductionForm from "../deduction-form/DeductionForm";
 import Confirmation from "../confirmation/Confirmation";
 import Success from "../success/Success";
 import Login from "../login/Login";
 import Auth from "../auth/Auth"
 import StatusList from "../status-list/StatusList";
-import isAuthenticated from "../auth/authenticationService";
-import {useCookies} from "react-cookie";
-
-
 
 const DeductionContainer = () => {
   const history = useHistory();
-
-  const PrivateRoute = ({ children, ...rest }) => {
-    const [cookies, setCookie, removeCookie] = useCookies(['access_cookie']);
-    const access_cookie = (cookies.hasOwnProperty('access_cookie')) ? cookies['access_cookie'] : '';
-
-    return isAuthenticated(access_cookie)
-      .then((auth_value) => {
-        if (auth_value) {
-          return (
-            <Route
-              {...rest}
-              render={({location}) => {
-                return children;
-              }}
-            />
-          );
-        }
-        else{
-          history.push("/home");
-        }
-      }
-    );
-  };
 
   return (<Switch>
       <Route exact path="/">
@@ -54,9 +27,9 @@ const DeductionContainer = () => {
       <Route path="/success">
           <Success/>
       </Route>
-      <PrivateRoute path="/status">
+      <Route path="/status">
           <StatusList/>
-      </PrivateRoute>
+      </Route>
       <Route path="/logout">
           <Login loginMock={() => history.push("/home")} />
       </Route>
